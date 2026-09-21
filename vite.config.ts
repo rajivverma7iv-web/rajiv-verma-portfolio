3982d5624a6fa3e265dc6871b198e2eb701d2adc
Rajiv Verma <rajivverma7iv-web@users.noreply.github.com>
2026-09-21 13:50:48 +0530

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/rajiv-verma-portfolio/',
   plugins: [react()],
   build: {
     rollupOptions: {
